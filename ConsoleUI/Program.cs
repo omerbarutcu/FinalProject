33a999db508356c244022bd337518c336878762b
ömer barutçu <omerbrtc@hotmail.com>
2023-12-26 21:09:48 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Concrete.InMemory;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(50,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
